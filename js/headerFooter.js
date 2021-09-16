152e93220c291cdbc30c12e85d3a1950287f82cf
D <MasterSifi>
2021-09-16 13:27:19 +0530

    correction

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -1,3 +1,9 @@
-	fetch('pages/header.html')
-	.then(response => response.text())
-	.then(data => document.querySelector("header").innerHTML = data);
+////insert header.html in header tag of index.html
+fetch('pages/header.html')
+.then(response => response.text())
+.then(data => document.querySelector("header").innerHTML = data);
+
+//insert footer.html in footer tag of index.html
+fetch('pages/footer.html')
+.then(response => response.text())
+.then(data => document.querySelector("footer").innerHTML = data);
