2c3bd8be35e52ad404b77a8e376ed20f23f8e7f0
MasterSifi <83062388+MasterSifi@users.noreply.github.com>
2021-09-16 18:36:13 +0530

    Update index.js
    OOP

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
-////insert header.html in header tag of index.html
-fetch('pages/header.html')
-.then(response => response.text())
-.then(data => document.querySelector("header").innerHTML = data);
+let index = {
 
-//insert footer.html in footer tag of index.html
-fetch('pages/footer.html')
-.then(response => response.text())
-.then(data => document.querySelector("footer").innerHTML = data);
+  //insert header.html in header tag of index.html
+  getHeader : function(){
+  fetch('pages/header.html')
+  .then(response => response.text())
+  .then(data => document.querySelector("header").innerHTML = data);
+  },
+  
+  //insert footer.html in footer tag of index.html
+  getFooter : function(){
+  fetch('pages/footer.html')
+  .then(response => response.text())
+  .then(data => document.querySelector("footer").innerHTML = data);
+  }
+}
+
+index.getHeader();
+index.getFooter();
